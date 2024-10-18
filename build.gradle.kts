4a03b818066dd1ef03ad097c51b79eee5c3158a5
FebiMaharani <147477650+FebiMaharani@users.noreply.github.com>
2024-10-18 19:56:32 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+dependencies {
+    implementation "androidx.compose.material:material-icons-extended:1.0.0"
+}
